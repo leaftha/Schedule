18b65fff542a736a92cb09794ccf763e1c90d933
leaftha <vltpcks@gmail.com>
2024-11-29 09:11:13 +0900

    firebase 설치

---
+++ src/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FireBase_apiKey,
+  authDomain: process.env.REACT_APP_FireBase_authDomain,
+  databaseURL: process.env.REACT_APP_FireBase_databaseURL,
+  projectId: process.env.REACT_APP_FireBase_projectId,
+  storageBucket: process.env.REACT_APP_FireBase_storageBucket,
+  messagingSenderId: process.env.REACT_APP_FireBase_messagingSenderId,
+  appId: process.env.REACT_APP_FireBase_appId,
+  measurementId: process.env.REACT_APP_FireBase_measurementId,
+};
+const app = initializeApp(firebaseConfig);
+
+//export default app;
+export const db = getDatabase(app);
