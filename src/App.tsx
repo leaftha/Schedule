9c283b9e74e9dde0884b6d7e2871da4faf47cb1a
SaChan <vltpcks@gmail.com>
2024-11-30 15:52:24 +0900

    스케줄 입력 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import LoggChect from "./LoggCheck";
+import AddSchedule from "./addSchedule";
 
 function App() {
   const [isLogged, setIsLogged] = useState<boolean>(false);
   return (
     <div>
       {isLogged ? (
-        <div></div>
+        <div>
+          <AddSchedule />
+        </div>
       ) : (
         <div>
           <h1>로그인</h1>
---
+++ src/addSchedule.tsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { ref, set } from "firebase/database";
+import { db } from "./firebase";
+import { uid } from "uid";
+
+const AddSchedule = () => {
+  const [scheduleType, setScheduleType] = useState<string>("주");
+  const [scheduleContent, setScheduleContent] = useState<string>("");
+  const inputSchedule = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const selectedDays = formData.getAll("days");
+    const uuid = uid();
+
+    const writeData = () => {
+      set(ref(db, "todo_days/" + uuid), {
+        selectedDays,
+        scheduleContent,
+        uuid,
+      });
+      setScheduleContent("");
+    };
+    writeData();
+  };
+  const changeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setScheduleContent(e.target.value);
+  };
+  return (
+    <div>
+      <form onSubmit={inputSchedule}>
+        <div>
+          <button type="button" onClick={() => setScheduleType("주")}>
+            주
+          </button>
+          <button type="button" onClick={() => setScheduleType("월")}>
+            월
+          </button>
+        </div>
+        {scheduleType === "주" ? (
+          <div>
+            <label>
+              월
+              <input type="checkbox" name="days" value="월" />
+            </label>
+            <label>
+              화
+              <input type="checkbox" name="days" value="화" />
+            </label>
+            <label>
+              수
+              <input type="checkbox" name="days" value="수" />
+            </label>
+            <label>
+              목
+              <input type="checkbox" name="days" value="목" />
+            </label>
+            <label>
+              금
+              <input type="checkbox" name="days" value="금" />
+            </label>
+            <label>
+              토
+              <input type="checkbox" name="days" value="토" />
+            </label>
+            <label>
+              일
+              <input type="checkbox" name="days" value="일" />
+            </label>
+            <label>할 일</label>
+          </div>
+        ) : (
+          <div>
+            <label>시작</label>
+            <input name="days" type="date" />
+            <label>끝</label>
+            <input name="days" type="date" />
+          </div>
+        )}
+        <input value={scheduleContent} onChange={changeInput} />
+        <button type="submit">추가</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddSchedule;
