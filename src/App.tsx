19dfee42e3994cb82003aa00cc27d46b5d989d30
SaChan <vltpcks@gmail.com>
2024-12-01 21:23:11 +0900

    fireabse에서 db통신

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import LoggChect from "./LoggCheck";
 import AddSchedule from "./addSchedule";
 import Calender from "./calender";
 import { ScheduleProvider } from "./scheduleProvider";
+import { ref, child, get } from "firebase/database";
+import { db } from "./firebase";
 
 function App() {
   const [isLogged, setIsLogged] = useState<boolean>(false);
+  const [weekData, setWeekData] = useState<object>({});
+  const [DayData, setDayData] = useState<object>({});
+  useEffect(() => {
+    if (!isLogged) return;
+
+    const dbRef = ref(db);
+    get(child(dbRef, "/todo_days"))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          setDayData(Array.isArray(data) ? data : Object.values(data));
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    get(child(dbRef, "/todo_week"))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val();
+          setWeekData(Array.isArray(data) ? data : Object.values(data));
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [isLogged]);
+
+  console.log(weekData);
+  console.log(DayData);
   return (
     <div>
       {isLogged ? (
